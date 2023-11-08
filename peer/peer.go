cb7f2f8203a5d9b9478d8ec2b290db721784fea5
teomormi <matteo.mormile@mail.polimi.it>
2023-11-08 11:48:25 +0100

    Update peer.go and created confFile

--- peer/peer.go
+++ peer/peer.go
@@ -2,10 +2,13 @@ package main
 
 import (
 	"context"
+	"encoding/csv"
+	"flag"
 	"fmt"
 	"log"
 	"math"
 	"net"
+	"os"
 	"strconv"
 
 	"google.golang.org/grpc"
@@ -23,27 +26,60 @@ type Peer struct {
 }
 
 var (
-	time     = 0 // Lamport variable
-	confFile = "confFile.txt"
+	my_row   = flag.Int("row", 1, "Indicate the parameters for this peer") // set with "-row <port>" in terminal
+	time     = 0                                                           // Lamport variable
+	confFile = "confFile.csv"
 )
 
 func main() {
 
+	flag.Parse()
+	// we will use a param passed through command line
 	// read from confFile.txt and set the values
 
-	peer := &Peer{
-		// edit this with new variables
-		name:    "peer",
-		address: "127.0.0.1",
-		portIn:  5000,
-		portOut: 50001,
+	// Apri il file CSV per la lettura
+	csvFile, err := os.Open(confFile)
+	if err != nil {
+		fmt.Printf("Error while opening CSV file: %v\n", err)
+		return
+	}
+	defer csvFile.Close()
+
+	reader := csv.NewReader(csvFile)
+	rows, err := reader.ReadAll()
+	if err != nil {
+		fmt.Printf("Error in reading CSV file: %v\n", err)
+		return
 	}
 
-	// open the port to new connection, when new one appen we have to create a connection also in the other way
-	StartListen(peer)
+	found := false
 
-	// Connect to the others client
-	connectToOthersPeer(peer)
+	for index, row := range rows {
+		if index == *my_row {
+			fmt.Printf("Row founded : %s, %s , %s\n", row[0], row[1], row[2])
+			found = true
+			break
+		}
+	}
+
+	if !found {
+		fmt.Printf("Riga not founded\n")
+	}
+	/*
+
+		peer := &Peer{
+			// edit this with new variables
+			name:    "peer",
+			address: "127.0.0.1",
+			portIn:  5000,
+			portOut: 50001,
+		}
+
+		// open the port to new connection, when new one appen we have to create a connection also in the other way
+		StartListen(peer)
+
+		// Connect to the others client
+		connectToOthersPeer()*/
 }
 
 func StartListen(peer *Peer) {
@@ -67,11 +103,37 @@ func StartListen(peer *Peer) {
 	}
 }
 
-func connectToOthersPeer(peer *Peer) {
+// Connect to others peer
+func connectToOthersPeer() {
+	// read csv file
+	file, err := os.Open(confFile)
+	if err != nil {
+		log.Fatalf("Failed to open configuration file: %v", err)
+	}
+	defer file.Close()
 
-	// Connect to others peer
+	reader := csv.NewReader(file)
+	rows, err := reader.ReadAll()
+	if err != nil {
+		log.Fatalf("Failed to read file data: %v", err)
+	}
 
-	serverConnection := connectToServer()
+	// array of peers
+	var peers []proto.MutualExlusionServiceClient
+
+	// foreach row in the
+	for index, row := range rows {
+		if len(row) < 2 || (index == *my_row) {
+			log.Printf("Skipped row: %v", row)
+			continue
+		}
+
+		peerAddress := row[0]
+		peerPort, _ := strconv.Atoi(row[1])
+
+		client := connectToPeer(peerAddress, peerPort)
+		peers = append(peers, client)
+	}
 
 	for {
 		var text string
@@ -79,23 +141,32 @@ func connectToOthersPeer(peer *Peer) {
 		fmt.Scanln(&text)
 
 		increaseTime() // an event occurred
+		/* Use array
+		for index, client := range clients {
+			response, err := client.AskPermission(context.Background(), &proto.Question{})
+			if err != nil {
+				log.Printf("RPC failed: %v", err)
+				continue
+			}
+			log.Printf("Response: %v", response)
+		}*/
 	}
 }
 
-func (c *Peer) AskPermission(ctx context.Context, in *proto.Question) (*proto.Answer, error) {
+func (peer *Peer) AskPermission(ctx context.Context, in *proto.Question) (*proto.Answer, error) {
 	// to do implement
 	return &proto.Answer{
 		Reply: false,
 	}, nil
 }
 
-func connectToPeer() proto.MutualExlusionServiceClient {
+func connectToPeer(address string, port int) proto.MutualExlusionServiceClient {
 	// Dial the server at the specified port.
-	conn, err := grpc.Dial(*serverAddr+":"+strconv.Itoa(*serverPort), grpc.WithTransportCredentials(insecure.NewCredentials()))
+	conn, err := grpc.Dial(address+":"+strconv.Itoa(port), grpc.WithTransportCredentials(insecure.NewCredentials()))
 	if err != nil {
-		log.Printf("Could not connect to peer at port %d", *serverPort)
+		log.Printf("Could not connect to peer %s at port %d", address, port)
 	} else {
-		log.Printf("Connected to the peer at port %d\n", *serverPort)
+		log.Printf("Connected to the peer %s at port %d\n", address, port)
 	}
 	return proto.NewMutualExlusionServiceClient(conn)
 }
